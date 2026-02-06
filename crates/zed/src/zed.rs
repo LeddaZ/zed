2c0a584784014222e9f8d810a503a3c46ba65d31
Leonardo Ledda <leonardoledda@gmail.com>
2026-02-06 09:19:31 +0100

    Disable unsupported GPU popup
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -572,7 +572,8 @@ pub fn initialize_workspace(app_state: Arc<AppState>, cx: &mut App) {
 
         if let Some(specs) = window.gpu_specs() {
             log::info!("Using GPU: {:?}", specs);
-            show_software_emulation_warning_if_needed(specs.clone(), window, cx);
+            // Disable unsupported GPU popup
+            // show_software_emulation_warning_if_needed(specs.clone(), window, cx);
             if let Some(crash_client) = cx.try_global::<CrashHandler>() {
                 crashes::set_gpu_info(&crash_client.0, specs);
             }
